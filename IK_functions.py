82a712fe6c6ad6e57aa3a1904a86d1a4f040eb67
Arturs Kurzemnieks <artursk@kth.se>
2019-09-12 18:19:38 +0200

    fml...

--- IK_functions.py
+++ IK_functions.py
@@ -59,15 +59,16 @@ class DH_Entry:
         self.r = r
         self.theta = theta
 
+# returns the DH table filled with values for a given joint configuration
 def get_kuka_DH(q):
     kuka_DH = [
-        DH_Entry(math.pi / 2, 0, 0, q[0]),
-        DH_Entry(-math.pi / 2, 0, 0, q[1]),
-        DH_Entry(-math.pi / 2, 0.4, 0, q[2]),
-        DH_Entry(math.pi / 2, 0, 0, q[3]),
-        DH_Entry(math.pi / 2, 0.39, 0, q[4]),
-        DH_Entry(-math.pi / 2, 0, 0, q[5]),
-        DH_Entry(0, 0, 0, q[6])
+        DH_Entry(math.pi / 2., 0.311, 0., q[0]), # add base offset as d
+        DH_Entry(-math.pi / 2., 0., 0., q[1]),
+        DH_Entry(-math.pi / 2., 0.4, 0., q[2]),
+        DH_Entry(math.pi / 2., 0., 0., q[3]),
+        DH_Entry(math.pi / 2., 0.39, 0., q[4]),
+        DH_Entry(-math.pi / 2., 0., 0., q[5]),
+        DH_Entry(0., 0.078, 0., q[6]), # add end effector offset as d
     ]
     return kuka_DH
 
@@ -78,7 +79,7 @@ def transform_to_prev(theta, d, alpha, r):
         [np.cos(theta), -np.sin(theta) * np.cos(alpha), np.sin(theta) * np.sin(alpha), r * np.cos(theta)],
         [np.sin(theta), np.cos(theta) * np.cos(alpha), -np.cos(theta) * np.sin(alpha), r * np.sin(theta)],
         [0, np.sin(alpha), np.cos(alpha), d],
-        [0, 0, 0, 1]
+        [0, 0, 0, 1.]
     ])
     return transform
 
@@ -91,57 +92,136 @@ def kuka_IK(point, R, joint_positions):
     """
     Fill in your IK solution here and return the seven joint values in q
     """
-    # forward kinematics for q
 
-    # eps_theta = 0.01 * np.ones(7)
-    # theta_hat = np.array(q) + eps_theta
-    # print theta_hat
+    # eps_theta = 0.1 * np.ones(len(joint_positions))
+    theta_hat = np.array(joint_positions)
 
-    # X = np.array([x, y, z, euler values from R])
-    # X_hat = kuka_FK(theta_hat)
-    # eps_X = X_hat - X
+    angle_X, angle_Y, angle_Z = get_euler_angles_from_T(np.array(R))
+    X = np.array([x, y, z, angle_X, angle_Y, angle_Z])
 
-    tolerance = 1e-5
+    X_hat, intermediary_transforms = kuka_FK(theta_hat)
+    X_hat
+    eps_X = X_hat - X
+
+    tolerance = 0.003
+
     # iterate the approximation until we get under the min tolerance
-    # while eps_X > tolerance
-    print(kuka_FK(joint_positions))
     
+    MAX_ITERATIONS = 100
+    iterations = 0
+    while np.mean(np.abs(eps_X)) >= tolerance and iterations < MAX_ITERATIONS:
+        J, J_inv = compute_jacobian(intermediary_transforms, theta_hat)
+        eps_theta = np.dot(J_inv, eps_X)
+        # if iterations % 50 == 0:
+        #     print(eps_theta)
+        # print(eps_theta)
+        theta_hat = theta_hat - eps_theta
+        
+        X_hat, intermediary_transforms = kuka_FK(theta_hat)
+        eps_X = X_hat - X
 
+        iterations = iterations + 1
+        # print(iterations)
+        # print(eps_X)
+
+    # print(X_hat)
+    q = theta_hat.tolist()
     return q
+
+def get_euler_angles_from_T(T):
+    # Euler angles from the rotation part of the matrix
+    #     | r11 r12 r13 |
+    # R = | r21 r22 r23 |
+    #     | r31 r32 r33 |
+
+    # Method as per https://walter.readthedocs.io/en/latest/Kinematics/
+    # and http://www.gregslabaugh.net/publications/euler.pdf
+    alpha = 0.
+    beta = 0.
+    gamma = 0.
+
+    # beta = -math.pi / 2.
+    # alpha = math.atan2(-T[0,1], -T[0,2])
+    # gamma = 0.
+
+    
+    if np.abs(T[2,0] + 1.) < 0.03:
+        # print("NEGATIVE")
+        beta = math.pi / 2.
+        alpha = math.atan2(T[0,1], T[0,2])
+        gamma = 0.
+    elif np.abs(T[2,0] - 1.) < 0.03:
+        # print("POSITIVE")
+        beta = -math.pi / 2.
+        alpha = math.atan2(-T[0,1], -T[0,2])
+        gamma = 0.
+    else:
+        print("BASECASE")
+        beta = -math.asin(T[2,0])
+        cosBeta = math.cos(beta)
+        alpha = math.atan2(T[2,1] / cosBeta, T[2,2] / cosBeta)
+        gamma = math.atan2(T[1,0] / cosBeta, T[0,0] / cosBeta)
+
+    print(alpha, beta, gamma)
+    return alpha, beta, gamma
+
 
 # Forward kinematics
 def kuka_FK(q):
 
     # end effector transform in the current frame
-    # initially translated 0.311 in z direction due to the base offset
     current_T = np.array([
-        [1, 0, 0, 0],
-        [0, 1, 0, 0],
-        [0, 0, 1, 0.311],
-        [0, 0, 0, 1]
+        [1., 0, 0, 0],
+        [0, 1., 0, 0],
+        [0, 0, 1., 0],
+        [0, 0, 0, 1.]
     ])
 
     # get the DH table values for the current q values
     DH_values = get_kuka_DH(q)
 
     # iterate through the joints
+    # save intermediary transforms needed for Jacobian
+    intermediaries = [current_T]
     for i in range(len(q)):
         link = DH_values[i] # get corresponding DH row
         transform = transform_to_prev(link.theta, link.d, link.alpha, link.r)
 
-        current_T = np.dot(current_T, transform)
+        current_T = np.matmul(current_T, transform)
+        intermediaries.append(current_T)
+
+    angle_X, angle_Y, angle_Z = get_euler_angles_from_T(current_T)
     
-    # + end effector offset
-    # current_T = 
+    X = [current_T[0,3], current_T[1,3], current_T[2,3], angle_X, angle_Y, angle_Z]
+    
+    return X, intermediaries
 
-    # Euler angles from the rotation part of the matrix
-    #     | r11 r12 r13 |
-    # R = | r21 r22 r23 |
-    #     | r31 r32 r33 |
+def compute_jacobian(transforms, q):
+    #     | Jp (3 x n) |
+    # J = | Jo ( 3 x n) |
 
-    angle_X = np.arctan2(current_T[2,1], current_T[2,2])
-    angle_Y = np.arctan2(-current_T[2,0], np.sqrt(np.square(current_T[2,1]) + np.square(current_T[2,2])))
-    angle_Z = np.arctan2(current_T[1,0], current_T[0,0])
+    # p are position vectors for the origin frames (link frames)
+    # p_e for the end effector frame
+    p_e = transforms[len(transforms) - 1][0:3,3] # get the (x,y,z) vector for the last transform
 
-    return [current_T[0,3], current_T[1,3], current_T[2,3], angle_X, angle_Y, angle_Z]
+    num_cols = len(q)
+    J = np.zeros((6, num_cols))
+    for i in range(num_cols):
+        # all joints revolute, so no need to cover prismatic case
+        
+        # linear:
+        # z_i-1 X (p_e - p_i-1)
+        p = transforms[i][0:3,3] # (x,y,z)
 
+        # z is unit vector of joint axis (z axis)
+        # so we need the rotation of z component
+        z = transforms[i][0:3,2]
+        z = z / np.sqrt(np.sum(np.square(z))) # normalize to get unit vec
+
+        linear = np.cross(z, p_e - p)
+
+        # angular is just z_i-1
+        column = np.array([linear[0], linear[1], linear[2], z[0], z[1], z[2]])
+        J[:,i] = column
+
+    return J, np.linalg.pinv(J)
